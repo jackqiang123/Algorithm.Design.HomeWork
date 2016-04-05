5e3c9e6040d3b02dd882d13cdcabcc64d35211f1
Lian Lu <lianlu@asu.edu>
2016-04-04 22:28:19 -0500

    HW1

---
+++ countInversion/Solution.java
@@ -0,0 +1,43 @@
+class Solution{
+	public static long findInversion(int []nums){
+		if (nums.length <= 1) return 0;
+		return findInversion(nums,0,nums.length-1);		
+	}
+
+	private static long findInversion(int []nums, int lo, int hi)
+	{
+		if (lo >= hi) return 0;
+		int mid = (lo + hi)/2;
+		return findInversion(nums, lo, mid) + findInversion(nums, mid + 1, hi) + quickCombine(nums, lo, hi, mid);
+	}
+
+	private static long quickCombine(int []nums, int lo, int hi, int mid){
+		long res = 0;
+		int temp[] = new int[hi - lo + 1];
+		int i = lo; 
+		int j = mid + 1;
+		int pos = 0;
+		while(pos < temp.length){
+			if (i <= mid && j <= hi)
+			{
+				if (nums[i] < nums[j])
+					temp[pos++] = nums[i++];
+				else {
+					res += (mid - i + 1);
+					temp[pos++] = nums[j++];
+				} 
+			}
+			else if (i <= mid)
+			{
+				temp[pos++] = nums[i++];
+			}
+			else {
+				temp[pos++] = nums[j++];
+			}
+		}
+		for (i = lo; i <= hi; i++)
+			nums[i] = temp[i-lo];
+		return res;
+	}
+
+}
